955dc11c0beb0eb574997564db7bb7471351a834
nchin <nc0372a@student.american.edu>
2022-06-10 20:34:47 -0400

    Functional todo list site

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_pic from "./assets/profile_pic.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img className="profile_image" src={profile_pic} alt="Profile Pic"></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">About me/Nancy Ching</div>
+            <div className="brief_description">
+              <p>Computer Science Student at American University (2024)</p>
+              <hr></hr>
+              <p>A little about myself:</p>
+              <ul>
+                <li>I love coding and one day would like to be a Software Engineer.</li>
+                <li>I love video games and also one day want to program an app or game.</li>
+                <li>I love IBM Accelerate!</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
